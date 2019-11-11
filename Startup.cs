118316014f754d73c09ab50395993deaf9ebf5d3
zameer khan <zameer.kh@gmail.com>
2019-11-11 21:21:39 +1100

    Add nav bar

--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,7 @@ namespace HMPieStore
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Pie}/{action=List}/{id?}");
             });
         }
     }
